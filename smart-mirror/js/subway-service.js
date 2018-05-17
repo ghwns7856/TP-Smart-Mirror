074afe98b56164ea1469f8bfaf953c38b9ef758f
Hoduji <ghwns7856@naver.com>
2018-05-17 12:24:07 +0900

    Update subway-service.js

--- smart-mirror/js/subway-service.js
+++ smart-mirror/js/subway-service.js
@@ -8,9 +8,6 @@
     var OTHERDAY_CODE = 1;
     service.stationrow = null;
 
-    /** 서울시 역코드로 서울시 지하철역별 열차 도착 정보 검색
-    * http://data.seoul.go.kr/openinf/openapiview.jsp?infId=OA-103&tMenu=11 */
-
     service.init = function(station){
       return $http.get("http://openAPI.seoul.go.kr:8088/"+config.subway.key+"/xml/SearchInfoBySubwayNameService/1/5/"+station+"/").
       then(function(response) {
